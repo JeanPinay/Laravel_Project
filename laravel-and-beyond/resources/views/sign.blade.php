ff53532046ff64dec5980355dd90f3ac8d7435d8
Livara <livara@gmail.com>
2023-04-18 20:58:44 +0200

    Update bootstrap template

--- laravel-and-beyond/resources/views/sign.blade.php
+++ laravel-and-beyond/resources/views/sign.blade.php
@@ -1,2 +1,19 @@
 <!-- TODO: detail page of every article  -->
+@extends('layout')
+@section('signs')
+<div class="container my-5 ">
+<div class="p-5 text-center " style="background-image: url('https://cf.ltkcdn.net/horoscopes/images/orig/287818-1600x1066-astrological-signs.jpg'); background-size: cover; background-position: center;">
+
+    <h1 class="text-body-emphasis" style="color: #000000;" id="">{{$sign->name}}</h1>
+    <p class="lead my-3" style="color: #000000;" id="description">
+    {{$sign->description}}
+    </p>
+    <div class="d-inline-flex gap-5 mb-5">
+    <button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top">
+        Tooltip on top
+</button>
+    </div>
+</div>
+</div>
+@endsection
 
